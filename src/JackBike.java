76585d1865504fbe0848d68aa0de9f6521cd8d0c
Jack Lay <42285595+jackhlay@users.noreply.github.com>
2023-09-10 17:55:25 -0600

    Added new bicycle class

---
+++ src/JackBike.java
@@ -0,0 +1,9 @@
+public class JackBike extends Bike{
+    public JackBike(String modelName){
+        super(modelName, "front");
+    }
+    public void ride(){
+        System.out.println("This bike can surely move");
+    }
+
+}
--- src/davisBike.java
+++
@@ -1,8 +0,0 @@
-public class davisBike extends Bike{
-    public davisBike(String modelName) {
-        super(modelName, "full");
-    }
-    public void ride(){
-        System.out.println("davisBike rides smooth with full suspension");
-    }
-}
