1327827f5b2f73be979029b3412ef38d7a843803
JTxCo <103444631+JTxCo@users.noreply.github.com>
2023-09-11 10:08:28 -0600

    added super doep subclass

---
+++ src/SuperDopeSickBike.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class SuperDopeSickBike extends Bike{
+    public SuperDopeSickBike(String modelName){
+        super(modelName, "full");
+    }   
+    public void ride(){
+        System.out.println("This bike is super dope sick skirt skirt");
+    }
+}
